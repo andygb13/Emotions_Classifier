3ce5096f32875aa1d230b8d4aa324d429ffc9880
Andy Guevara <68925048+andygb13@users.noreply.github.com>
2022-10-06 18:57:46 -0600

    added comments and sample image

--- live_emotions.py
+++ live_emotions.py
@@ -7,20 +7,26 @@ import pandas as pd
 import torch
 import matplotlib.pyplot as plt
 
-#$pip install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
 
+#Install yolov5 requirements prior to using this script
+#pip install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
+
+#Classifier file path in local computer.
 cl_path = r'C:\Users\andyg\Documents\ECE 499 - CV Project\Live Implementation\best_model40'
+#Object detector weights file path in local computer
 ob_path = r'C:\Users\andyg\Documents\ECE 499 - CV Project\Live Implementation\best.pt'
 
+#Emotions classification model
 cl_model = keras.models.load_model(cl_path)
-
+#Object detection model
 od_model = torch.hub.load('ultralytics/yolov5', 'custom', path=ob_path)
 
 def NormalizeData(data):
+    #Normalize image values from to range (0,1)
     return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 def process_img(sample):
-    #input an image to do desired processing
+    #Process input image to satisfy classifier input shape
     if len(sample.shape) > 2:
         sample = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
     img = NormalizeData(sample)
@@ -31,6 +37,7 @@ def process_img(sample):
     return img
 
 def get_eyes(img):
+    #Perform object detection and output bounding box coordinates
     results = od_model(img)
     results = results.pandas().xyxy[0]
     if len(results) == 1:
@@ -49,13 +56,15 @@ def get_eyes(img):
     return x1,x2,y1,y2,eyes,flag
 
 def find_emotion(eyes):
+    #Predict an emotion unsing classifier and output a prediction in text form
     eyes = process_img(eyes)
     classes = np.asarray(['Anger', 'Contempt', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise'])
     results = cl_model.predict(eyes)
     prediction = classes[results[0,:] == np.max(results[0,:])]
     return prediction[0]
 
-cam = cv2.VideoCapture(0)
+#Perform live implementation frame by frame using the functions declared previously
+cam = cv2.VideoCapture(0) #Video source 0 for system default source
 while True:
     if cam.isOpened():
         ret, frame = cam.read()
